629714a835d04fbf3198b83cccf90b593fbd4259
maeveBestDev <77807261+maeveBestDev@users.noreply.github.com>
2021-03-05 11:12:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function RedirectionJavascript(){
-  document.location.href="https://pixees.fr/informatiquelycee/n_site/nsi_prem_html_css.html"; 
+
+function controle(){
+var saisie =document.getElementById("input").value;
+if (saisie == "anime"){
+  document.location.href="https://pixees.fr/informatiquelycee/n_site/nsi_prem_html_css.html";
+  }
+if (saisie == "jeux"){
+    document.location.href="https://www.w3schools.com/tags/ev_onsearch.asp";
+  }
+if(saisie == "marrant"){
+  document.location.href="http://memo-web.fr/categorie-javascript-120/";
+}
+
 }
