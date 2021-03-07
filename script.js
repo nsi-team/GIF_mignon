35bbf9eb0b26429867f3a6be2b91855bfb3152c9
maeveBestDev <77807261+maeveBestDev@users.noreply.github.com>
2021-03-07 17:46:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 function controle(){
 var saisie =document.getElementById("input").value;
-if (saisie == "anime" || "animé" || "Anime" || "Animé"){
+if (saisie == "anime" || "Anime"){
   document.location.href="https://nsi-team.github.io/Anime/";
   }
 if (saisie == "jeux vidéos" || "Jeux vidéos"){
     document.location.href="https://nsi-team.github.io/Jeux_Videos/";
   }
-if(saisie == "pixel art" || "Pixel Art" || "Pixel art"){
+if(saisie == "pixel art" || "Pixel art"){
   document.location.href="https://nsi-team.github.io/Pixel_Art/";
 }
 if(saisie == "marrant" || "Marrant"){
